e6e0f45823cebd1dea93a2e585a55c45a58cd774
hieuvn <hieuvn@smartosc.com>
2024-12-05 16:16:18 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import { Inter, Manrope } from "next/font/google";
 import Header from "@/src/components/Header";
 import Footer from "../components/Footer";
 import siteMetadata from "../utils/siteMetaData";
-import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -63,13 +62,6 @@ export default function RootLayout({ children }) {
           "font-mr bg-light dark:bg-dark"
         )}
       >
-        <Script id="theme-switcher" strategy="beforeInteractive">
-          {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }`}
-        </Script>
         <Header />
         {children}
         <Footer />
