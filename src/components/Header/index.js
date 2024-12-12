72a9900c2b4bec2fe638377ace3898f4bd5ab6a9
hieuvn <hieuvn@smartosc.com>
2024-12-12 15:28:17 +0700

    portfolio tab

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -55,6 +55,7 @@ const toggle = () =>{
             <Link href="/" className="mr-2">Home</Link>
             <Link href="/about" className="mx-2">About</Link>
             <Link href="/contact" className="mx-2">Contact</Link>
+            <Link href="http://portfolio.hiusvu.io.vn/" className="mx-2">Portfolio</Link>
             <button onClick={() => setMode(mode === "light" ? "dark" : "light")  }
             className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", mode === "light" ? "bg-dark text-light" :
             "bg-light text-dark" )}
@@ -72,6 +73,7 @@ const toggle = () =>{
             <Link href="/" className="mr-2">Home</Link>
             <Link href="/about" className="mx-2">About</Link>
             <Link href="/contact" className="mx-2">Contact</Link>
+            <Link href="http://portfolio.hiusvu.io.vn/" className="mx-2">Portfolio</Link>
             <button onClick={() => setMode(mode === "light" ? "dark" : "light")  }
             className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", mode === "light" ? "bg-dark text-light" :
             "bg-light text-dark" )}
